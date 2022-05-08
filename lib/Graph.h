696911c9c7e848c7f8d38cf9f5088042bf739f4d
Timothee Chabat <timo.chabat@gmail.com>
2022-05-08 13:20:34 +0200

    Initial commit

---
+++ lib/Graph.h
@@ -0,0 +1,13 @@
+#ifndef adagiGraph_h
+#define adagiGraph_h
+
+namespace adagi {
+class Graph
+{
+public:
+  Graph();
+  ~Graph();
+};
+}
+
+#endif // adagiGraph_h
---
+++ test/TestGraph.cxx
@@ -0,0 +1,10 @@
+#include <Graph.h>
+
+#include <cassert>
+
+int main(int argc, char* argv[])
+{
+  adagi::Graph graph;
+
+  return 0;
+}
